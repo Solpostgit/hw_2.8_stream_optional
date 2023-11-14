27e901c34a10235b453565e773291e16ff11ae29
Solpostgit <begemot_off@mail.ru>
2023-11-14 20:39:09 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/hw_2/_stream_optional/Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.hw_2._stream_optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/hw_2/_stream_optional/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.hw_2._stream_optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
